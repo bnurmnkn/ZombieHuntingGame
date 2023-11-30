29690ba3c781f5a760a7e396123ddf5884f4e2d6
Beyza Nur MANKAN <94225615+bnurmnkn@users.noreply.github.com>
2023-11-30 19:06:22 +0300

    menu

--- Assets/Scripts/HomeMenu.cs
+++ Assets/Scripts/HomeMenu.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HomeMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayButton()
     {
-        
-    }
+        int nextIndex=SceneManager.GetActiveScene().buildIndex+1;
+        SceneManager.LoadScene(nextIndex);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
