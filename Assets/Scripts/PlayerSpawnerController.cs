c9225ce55fc6a73df7df4b3547e3b14ee5a01844
Beyza Nur MANKAN <94225615+bnurmnkn@users.noreply.github.com>
2023-12-14 14:36:36 +0300

    scripts_PlayerController

--- Assets/Scripts/PlayerSpawnerController.cs
+++ Assets/Scripts/PlayerSpawnerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerSpawnerController : MonoBehaviour
 {
     float playerSpeed=5f;
+    float LeftRightSpeed;
     void Start()
     {
         
@@ -13,7 +14,23 @@ public class PlayerSpawnerController : MonoBehaviour
     
     void Update()
     {
-        Vector3 playerNewPosition=new Vector3(transform.position.x-playerSpeed*Time.deltaTime,transform.position.y,transform.position.z);
+        float touchX=0;
+        float newXValue=0;
+        if(Input.touchCount>0 && Input.GetTouch(0).phase==TouchPhase.Moved)
+        {
+            LeftRightSpeed=250f;
+            
+            touchX=Input.GetTouch(0).deltaPosition.x/Screen.width;
+
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            LeftRightSpeed=250f;
+            touchX=Input.GetAxis("Mouse X");
+
+        }
+        newXValue=transform.position.z+LeftRightSpeed*touchX*Time.deltaTime;
+        Vector3 playerNewPosition=new Vector3(transform.position.x-playerSpeed*Time.deltaTime,transform.position.y,newXValue);
         transform.position=playerNewPosition;
     }
 }
